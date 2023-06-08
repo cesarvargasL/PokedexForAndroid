ad805fb3a1785a8e898682211a62c3f6caef2212
César L Vargas Lezano <cesarluisvargaslezano@gmail.com>
2023-06-08 01:56:31 -0400

    Commit of the image main

--- app/src/main/java/com/example/pokedex/Home.kt
+++ app/src/main/java/com/example/pokedex/Home.kt
@@ -3,10 +3,9 @@ package com.example.pokedex
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_home)
     }
 }
